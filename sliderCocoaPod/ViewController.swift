07f6df5dbe1c8db3e5cbaa4ea3f2f207abba965f
DanielMoraCSG <daniel@csgroup.mx>
2016-11-18 20:57:38 -0600

    Initial Commit

---
+++ sliderCocoaPod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sliderCocoaPod
+//
+//  Created by Daniel Mora Osorio on 18/11/16.
+//  Copyright © 2016 CodecMedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
